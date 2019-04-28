fd674f66175f1a8272563d99c06a9c9961022af5
Gabriel Henrique Stamato <stamato7@gmail.com>
2019-04-28 15:26:14 -0300

    teste

--- app/Controllers/Http/OrderController.js
+++ app/Controllers/Http/OrderController.js
@@ -88,28 +88,6 @@ class OrderController {
   async payment({ params, request, response }) {
     const order = await Order.find(params.id);
 
-    const { formPay } = request.only(["formPay"]);
-
-    let { money } = request.only(["money"]);
-
-    let troco;
-
-    switch (formPay) {
-      case "cash":
-        troco = money - order.total_comanda;
-        if (troco < 0) {
-          return response.json({
-            status: error,
-            message: `Ainda faltam ${troco}`
-          });
-          break;
-        }
-
-      case "card":
-        money = order.total_comanda;
-        break;
-    }
-
     await Database.table("orders")
       .where("id", order.id)
       .update("close", true);
